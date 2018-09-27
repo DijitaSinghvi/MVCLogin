3066e89644edd230b168e01ea48fc9ff2463faf3
DijitaSinghvi <42962070+DijitaSinghvi@users.noreply.github.com>
2018-09-27 16:05:23 +0530

    Added controller and views for item model.

--- Memberships/Entities/Item.cs
+++ Memberships/Entities/Item.cs
@@ -31,7 +31,7 @@ namespace Memberships.Entities
         public int SectionId { get; set; }
         public int PartId { get; set; }
         public int ItemTypeId { get; set; }
-        public int IsFree { get; set; }
+        public bool IsFree { get; set; }
         [DisplayName("Item Types")]
         public ICollection<ItemType> ItemTypes { get; set; }
         [DisplayName("Sections")]
